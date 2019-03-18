61d783c1923494cdc709025ce25f86aea1e2bebe
hallowf <alexandre.cortegaca@gmail.com>
2019-03-18 15:57:08 +0000

    Fixed build scripts

--- scripts/lin_before_install.sh
+++ scripts/lin_before_install.sh
@@ -1,5 +1,5 @@
-sudo apt update && sudo apt upgrade
+sudo apt update
 sudo apt install make cmake gcc libgtk-3-dev \
-libwebkitgtk-dev libwebkitgtk-3.0-dev libgstreamer-gl1.0-0 \
+libwebkitgtk-dev libwebkitgtk-3.0-dev \
 freeglut3 freeglut3-dev python-gst-1.0 python3-gst-1.0 \
 libglib2.0-dev libgstreamer-plugins-base1.0-dev
