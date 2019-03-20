f45ceeca597ca2f1fda6bb2f00ed438fd2158d24
hallowf <alexandre.cortegaca@gmail.com>
2019-03-20 01:17:59 +0000

    Added input checking

--- scs/gui/home.py
+++ scs/gui/home.py
@@ -5,10 +5,10 @@ from backend.scs_classes import Backend
 
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
-        # Backend stuff --------------------
+        # Backend stuff ----------------------------------
         self.scs_backend = None
         self.cheats_file = None
-        # Main components ------------------
+        # Main components ---------------------------------
         wx.Frame.__init__(self, parent, title=title, size=(500,600))
         self.CreateStatusBar() # A Statusbar in the bottom of the window
         panel = wx.Panel(self) # Main Panel
@@ -28,20 +28,17 @@ class MainWindow(wx.Frame):
         self.SetMenuBar(menu_bar)  # Adding the MenuBar to the Frame content.
 
 
-        ## Content ---------------------
+        ## Content ---------------------------------------
 
         # create some sizers
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         grid = wx.GridBagSizer(hgap=5, vgap=5)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
 
-
-
         # Logger
         self.logger = wx.TextCtrl(self, size=(300,225), style=wx.TE_MULTILINE | wx.TE_READONLY)
         self.logger.SetBackgroundColour((3, 62, 78))
         self.logger.SetForegroundColour((172, 180, 182))
-
 
         # add a spacer to the sizer
         grid.Add((10, 20), pos=(0,0))
@@ -68,7 +65,6 @@ class MainWindow(wx.Frame):
         self.game_name_input = wx.TextCtrl(self, value="", size=(140,-1))
         grid.Add(self.game_name_input, pos=(3,1))
         
-
         # Terminate command input
         self.end_command = wx.StaticText(self, label="Terminate shortcut:")
         self.end_command.SetForegroundColour("MIDNIGHT BLUE")
@@ -79,14 +75,15 @@ class MainWindow(wx.Frame):
         # another spacer
         grid.Add((10, 20), pos=(5,0))
 
+        # Hook button
+        self.hook_button = wx.Button(self, label="Hook")
+        self.Bind(wx.EVT_BUTTON, self.on_hook,self.hook_button)
+        grid.Add(self.hook_button, pos=(6,0))
 
-        # Run button
-        self.run_button = wx.Button(self, label="Run")
-        self.Bind(wx.EVT_BUTTON, self.on_run,self.run_button)
-        grid.Add(self.run_button, pos=(6,0))
-
-
-
+        # Unhook button
+        self.unhook_button = wx.Button(self, label="Unhook")
+        self.Bind(wx.EVT_BUTTON, self.on_unhook,self.unhook_button)
+        grid.Add(self.unhook_button, pos=(6,1))
 
         hSizer.Add(grid, 0, wx.ALL, 5)
         hSizer.Add(self.logger)
@@ -97,11 +94,10 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.on_about, menu_about)
         self.Bind(wx.EVT_MENU, self.on_exit, menu_exit)
 
-
-
         # Show
         self.Show()
 
+    # File dialog for cheats file
     def on_open_cheats(self, e):
          # otherwise ask the user what new file to open
         with wx.FileDialog(self, "Open cheats file", wildcard=".json files (*.json)|*.json",
@@ -110,25 +106,63 @@ class MainWindow(wx.Frame):
                 return     # the user changed their mind
             # get the path
             self.cheats_file = fileDialog.GetPath()
-            self.logger.AppendText("Path of cheats file is:\n%s" % self.cheats_file)
+        self.logger.AppendText("Path of cheats file is:\n%s\n" % self.cheats_file)
 
-    def on_run(self,e):
-        game_name = self.game_name_input.GetLineText(0)
-        proc_name = self.proc_name_input.GetLineText(0)
+    # Check user inputs
+    def check_inputs(self):
+        inputs = {}
+        game_name = self.game_name_input.GetLineText(0).strip()
+        proc_name = self.proc_name_input.GetLineText(0).strip()
+        inputs.update({
+            "Game name":game_name,
+            "Process name":proc_name,
+            "Cheats file":self.cheats_file
+            })
+        for value in inputs:
+            if not inputs[value] or inputs[value] == "" or inputs[value] == " ":
+                self.logger.AppendText("Missing value: %s" % value)
+                return False
+        return True
+
+    # To rehook hotkeys
+    def on_re_hook(self,e):
+        game_name = self.game_name_input.GetLineText(0).strip()
+        if not self.scs_backend:
+            self.logger.AppendText("Keyboard not hooked..\n")
+        else:
+            self.logger.AppendText('"Re-hooking" shortcuts\n')
+            self.scs_backend.re_hook_keys(game_name)
+
+    # This unhooks the keyboard and terminates the backend
+    def on_unhook(self,e):
+        if not self.scs_backend:
+            self.logger.AppendText("Keyboard not hooked..\n")
+        else:
+            self.logger.AppendText('"Un-hooking" shortcuts and terminating backend\n')
+            self.scs_backend.unhook_keys()
+            self.scs_backend = None
+
+    # To hook the keyboard
+    def on_hook(self,e):
+        if not self.check_inputs():
+            return
+        game_name = self.game_name_input.GetLineText(0).strip()
+        proc_name = self.proc_name_input.GetLineText(0).strip()
         try:
-            self.backend = Backend(proc_name, self.cheats_file, game_name)
+            self.logger.AppendText("Starting backend, parameters:\n\tProcess:%s\n\tGame name:%s\n" % (proc_name, game_name))
+            self.scs_backend = Backend(proc_name, self.cheats_file, game_name)
         except Exception as e:
             e_name = e.__class__.__name__
             e_str = ""
             if e_name == "CheatsMissing":
-                e_str = "Failed to find cheats file"
+                e_str = "Failed to find cheats file at %s" % self.cheats_file
             elif e_name == "KeyError":
                 e_str = "Failed to found %s in cheats" % game_name
             elif e_name == "ProcessError":
                 e_str = "Failed to access process %s" % proc_name
             else:
                 e_str = str(e)
-            self.logger.AppendText(e_str)
+            self.logger.AppendText(e_str + "\n")
 
     def on_about(self, e):
         dlg = wx.MessageDialog( self, "SCS - Shortcut Cheat System, built with python and wxpython", "About SCS", wx.OK)
